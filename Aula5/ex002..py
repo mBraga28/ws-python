fc42f87458e9be3732e998cd6e486d13389d3e2a
Marco Braga <marcoabo8@hotmail.com>
2021-05-17 23:33:59 -0300

    commit 1

---
+++ Aula5/ex002..py
@@ -0,0 +1,10 @@
+nome = input('Digite seu nome: ')
+print(nome)
+
+nome = input('Digite seu nome: ')
+print('É um prazer em te conhecer',nome)
+
+nome = input('Digite seu nome: ')
+print('É um prazer em te conhecer, {}!'.format(nome))
+#  |-------------------------------|
+#  |-------> semelhante ao System.out.prinf do java
