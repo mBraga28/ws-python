1143d0ccafa12dc0fe00f00f8cc6b4b803ab1943
Marco Braga <marcoabo8@hotmail.com>
2021-05-17 22:59:29 -0300

    Initial commit

---
+++ Aula4/DesafioAula4_1_3.py
@@ -0,0 +1,8 @@
+print('========= DESAFIO 03=========')
+n1 = input('Digite o primeiro número: ')
+n2 = input('Digite o segundo número: ')
+resultado = 0
+resultado = n1 + n2
+
+print(resultado)
+
---
+++ Aula4/aula4.py
@@ -0,0 +1,22 @@
+>>> print('Olá mundo!!!!')
+Olá mundo!!!!
+>>> print ('chupa minha bila')
+chupa minha bila
+>>> print ('huheuheuhuehuehuehueh')
+huheuheuhuehuehuehueh
+>>> a=7
+>>> b=4
+>>> c=9
+>>> resultado=(a+(a*b*c)*c-(a+a+b))
+>>> print(resultado)
+2257
+>>> nome='marco'
+>>> idade=25
+>>> peso=82.3
+>>> print(nome, idade, peso)
+marco 25 82.3
+>>> print('Nome: ', nome,'idade: ', idade, 'peso: ', peso)
+Nome:  marco idade:  25 peso:  82.3
+>>> print(a)
+7
+>>>  
